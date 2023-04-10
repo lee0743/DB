18b7a87927e7e3bc590c8f2befe514414b600f92
lee0743 <58101404+lee0743@users.noreply.github.com>
2023-04-10 12:22:32 +0900

    Update chap03_ex1.sql

--- chap03_ex1.sql
+++ chap03_ex1.sql
@@ -1,29 +1,42 @@
-/* µµ¼­¹øÈ£°¡ 1ÀÎ µµ¼­ÀÇ ÀÌ¸§ */ 
+/* ë„ì„œë²ˆí˜¸ê°€ 1ì¸ ë„ì„œì˜ ì´ë¦„ */ 
 SELECT bookname 
 FROM Book 
 WHERE bookid = 1;
 
-/* °¡°İÀÌ 20000¿ø ÀÌ»óÀÎ µµ¼­ÀÇ ÀÌ¸§ */ 
+/* ê°€ê²©ì´ 20000ì› ì´ìƒì¸ ë„ì„œì˜ ì´ë¦„ */ 
 SELECT bookname
 FROM Book
 WHERE price >= 20000;
 
-/* ¹ÚÁö¼ºÀÇ ÃÑ ±¸¸Å¾× */
+/* ë°•ì§€ì„±ì˜ ì´ êµ¬ë§¤ì•¡ */
 SELECT SUM(saleprice)
 FROM Orders
 WHERE custid = 1;
 
-/* ¹ÚÁö¼ºÀÇ ÃÑ ±¸¸Å¾× 2*/
+/* ë°•ì§€ì„±ì˜ ì´ êµ¬ë§¤ì•¡ 2*/
 SELECT SUM(saleprice)
 FROM Orders, Customer
-WHERE Orders.custid = Customer.custid AND Customer.name LIKE '¹ÚÁö¼º'; 
+WHERE Orders.custid = Customer.custid AND Customer.name LIKE 'ë°•ì§€ì„±'; 
 
-/* ¹ÚÁö¼ºÀÌ ±¸¸ÅÇÑ µµ¼­ÀÇ ¼ö */
+/* ë°•ì§€ì„±ì´ êµ¬ë§¤í•œ ë„ì„œì˜ ìˆ˜ */
 SELECT COUNT(*)
 FROM Orders
 WHERE custid = 1;
 
-/* ¹ÚÁö¼ºÀÌ ±¸¸ÅÇÑ µµ¼­ÀÇ ÃâÆÇ»çÀÇ ¼ö */
+/* ë°•ì§€ì„±ì´ êµ¬ë§¤í•œ ë„ì„œì˜ ì¶œíŒì‚¬ì˜ ìˆ˜ */
 SELECT COUNT(publisher)
 FROM Orders, Customer, Book
-WHERE Book.bookid = Orders.bookid AND Orders.custid = Customer.custid AND Customer.name LIKE '¹ÚÁö¼º'; 
+WHERE Book.bookid = Orders.bookid AND Orders.custid = Customer.custid AND Customer.name LIKE 'ë°•ì§€ì„±'; 
+
+/* ë°•ì§€ì„±ì´ êµ¬ë§¤í•œ ë„ì„œì˜ ì´ë¦„, ê°€ê²©, ì •ê°€ì™€ íŒë§¤ê°€ê²©ì˜ ì°¨ì´ */
+SELECT bookname, price, price - saleprice
+FROM Customer, Orders, Book
+WHERE Customer.custid = Orders.custid AND Orders.bookid = Book.bookid AND Customer.name LIKE 'ë°•ì§€ì„±';
+
+/* ë°•ì§€ì„±ì´ êµ¬ë§¤í•˜ì§€ ì•Šì€ ë„ì„œì˜ ì´ë¦„ */
+SELECT bookname
+FROM book
+WHERE NOT EXISTS(
+    SELECT bookname
+    FROM customer, orders
+    WHERE customer.custid = orders.custid AND orders.bookid = book.bookid AND customer.name LIKE 'ë°•ì§€ì„±');
